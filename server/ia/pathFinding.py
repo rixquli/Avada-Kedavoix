2aca60e4cc0b3586efe6f1ac79a705e926e1844e
clement bonnaud <clement.bonnaud@epita.fr>
2025-11-18 23:12:06 +0100

    feat: commentaires

--- server/ia/pathFinding.py
+++ server/ia/pathFinding.py
@@ -3,11 +3,15 @@ from client.classes.hitbox import HitBox
 
 
 class Pix:
+    """
+    pour stocker les donnes d'un pixel et y acceder plus vite
+    (position/ distance/ chemin pour y arriver)
+    """
     def __init__(self, pos: tuple[float, float], dist_e: float=0):
         self.x = int(pos[0])
         self.y = int(pos[1])
-        self.dist = 0
-        self.dist_e = dist_e
+        self.dist = 0           # distance a parcourir entre la position de depart et le pixel
+        self.dist_e = dist_e    # distance a vol d'oiseau entre le pixel et l'arrivee
         self.origin = None
         self.len_path = 0
 
@@ -20,15 +24,18 @@ class Pix:
                 return True
         return False
 
-    def get_tuple(self):
+    def get_tuple(self) -> tuple[float, float]:
+        """renvois la position"""
         return self.x, self.y
 
     def add_origin(self, pix, dist):
+        """ajoute un pixel a l'origine et calcule la distance"""
         self.origin = pix
         self.dist += pix.dist + dist
         self.len_path = pix.len_path + 1
 
     def get_path(self)  -> list[tuple[int, int]]:
+        """renvois le chemin j'usqua la position de depart"""
         if self.origin == None:
             return []
         path = self.origin.get_path()
@@ -37,6 +44,10 @@ class Pix:
 
 
 class Path:
+    """
+    pour calculer et stocker le chemin (precision representes le saut entre chaque pixel du chemin et
+    la distance minimale pour considerer l'arrivee)
+    """
     def __init__(self, pos: tuple[float, float], dest: tuple[float, float], hitbox: HitBox, precision: int = 1):
         self.pos = tuple((int(pos[0]),int(pos[1])))
         self.dest = tuple((int(dest[0]),int(dest[1])))
@@ -51,6 +62,7 @@ class Path:
         self.dest = tuple((int(x),int(y)))
 
     def dist_euclide(self, x: int, y: int, x_target: int = None, y_target: int = None) -> float:
+        """renvois la distance a vol d'oiseau (si x_target et y_target non remplis ils sont mis a l'arrivee)"""
         if x_target is None:
             x_target = self.dest[0]
         if y_target is None:
@@ -61,6 +73,10 @@ class Path:
         return pix.dist + pix.dist_e
 
     def adj(self, pos: tuple[int, int]) -> list[tuple[tuple[int, int], float]]:
+        """
+        renvois les voisins d'une position et leurs distance a la position
+        voisins = [(position(voisin), distance(voisin))]
+        """
         adj_list: list[tuple[tuple[int, int], float]] = list()
         x,y = pos
         for dx in(-self.precision, 0, self.precision):
@@ -70,19 +86,27 @@ class Path:
         return adj_list
 
     def search(self, nb_frame: int) -> list[tuple[int, int]]|None:
+        """
+        renvois les nb_frame positions du chemin vers la destination
+        (si nb_frame = 5 renvois 5 positions)
+        a reexecuter lorsque le chemin est vide
+        """
         visited = list()
         to_visit = list()
         to_visit.append(Pix(self.pos,self.dist_euclide(self.pos[0],self.pos[1])))
         nb = 0
+
         while len(to_visit)>0 and nb < 1000:
             #str_to_visit = list()
             #for pix in to_visit:
             #    str_to_visit.append(pix.get_tuple())
+
             nb += 1
             current_pix = to_visit[0]
 
-
             for pix in to_visit:
+                # prendre le pixel avec la distance la plus courte
+                # (distance pour aller au pixel + distance euclidienne arrive)
                 if self.dist(pix) < self.dist(current_pix) or (
                         self.dist(pix) == self.dist(current_pix) and
                         pix.dist_e < current_pix.dist_e):
@@ -97,18 +121,21 @@ class Path:
                 self.hitbox.update(pix.x, pix.y)
 
                 if self.dist_euclide(pix.x, pix.y) <= self.precision or nb_frame < pix.len_path:
-                    print(self.dist_euclide(pix.x, pix.y) <= self.precision)
+                    # test si il est arrive ou si il a calcule asse d'image
                     return pix.get_path()
 
                 elif pix.get_tuple() not in visited and not pix.in_list(to_visit) and not self.hitbox.get_collided():
+                    # ajoutes que les pixels non trouve et particable a la liste
                     to_visit.append(pix)
 
         return []
 
     def find_path(self, nb_frame: int = 5):
+        """met a jour le chemin en en calculant un nouveau"""
         self.path = self.search(nb_frame)
 
     def follow_path(self):
+        """renvois la direction de la prochaine position"""
         if len(self.path) == 0:
             return 0,0
         dx = self.path[0][0] - self.pos[0]
