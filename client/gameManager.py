0b92014ac83f465178697dcb9fe12b39787b2792
clement bonnaud <clement.bonnaud@epita.fr>
2026-03-18 02:34:11 +0100

    fix: bug of merge from fighting_systems

--- client/gameManager.py
+++ client/gameManager.py
@@ -30,7 +30,7 @@ from client.classes.pnj import PNJ
 from client.classes.wall import Wall
 from client.layerList import Layer
 from client.spellsManager import SpellsManager
-#from client.voice.realtimeVoice import get_voice_command, start_voice_recognition
+from client.voice.realtimeVoice import get_voice_command, start_voice_recognition
 
 
 # To import module from other folder
@@ -65,7 +65,7 @@ class GameManager:
         self.client_manager = ClientManager(self)
 
         # Setup voice recognition
-        #start_voice_recognition()
+        start_voice_recognition()
 
         self.spellManager = SpellsManager(self)
 
@@ -150,7 +150,7 @@ class GameManager:
             if event.type == pygame.QUIT:
                 self.running = False
             self.handle_event(event)
-        #self.handle_voice_event()
+        self.handle_voice_event()
 
         self.screen.fill((0, 0, 0))  # Dessine le fond noir
 
