45fc0c3782b87f4301ad2cb3663af6f5c50021ad
Theo Privat <etintrof66@gmail.com>
2026-05-14 16:01:45 +0200

    fix: remove debug mode for hitbox

--- client/classes/hitbox.py
+++ client/classes/hitbox.py
@@ -17,7 +17,7 @@ class HitBox(pygame.sprite.Sprite):
         w: int,
         h: int,
         world_layer: int | Layer = Layer.OVERWORLD,
-        debug: bool = True,
+        debug: bool = False,
     ):
         super().__init__()
         self.w = int(w)
